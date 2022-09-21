80db0603178a839b7202de4798e26cedb932286b
Holly Parker <hollygcochran@gmail.com>
2022-09-20 20:09:51 -0500

    favorites model

--- models/favorites.js
+++ models/favorites.js
@@ -1,3 +1,35 @@
 // ** Will contain:
 //   - User ID 
 //   - Strain ID
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Favorites extends Model {}
+
+Favorites.init(
+    {
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'users',
+                key: 'id'
+                }
+    },
+        strain_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'strain',
+                key: 'id'
+                }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'strain'
+    }
+);
+
+module.exports = favorites
