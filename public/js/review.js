8edb50709da2632867867fa43e8191ed007a98d6
Grayson Harvey <graycodes.nu@gmail.com>
2022-09-25 20:44:46 -0500

    restructuring for connectivity across files.

--- public/js/review.js
+++ public/js/review.js
@@ -1,22 +1,17 @@
-// register handlebars helper 
-window.Handlebars.registerHelper('select', function( value, options ){
-  var $el = $('<select />').html( options.fn(this) );
-  $el.find('[value="' + value + '"]').attr({'selected':'selected'});
-  return $el.html();
-});
-
-
 // user clicks button, toggles between hiding and showing dropdown content 
 function dropdown() {
   document.getElementById('myDropdown').classList.toggle('show');
+  console.log('reading!');
 }
+
 
 // close dropdown menu if user clicks outside of it 
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
+    console.log('getting dropdown content');
     let dropdowns = document.getElementsByClassName('dropdown-content');
     let i;
-    for (i = 0 i < dropdowns.length; i++) {
+    for (i = 0; i < dropdowns.length; i++) {
       let (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
