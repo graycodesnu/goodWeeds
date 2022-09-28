105d2db25fa5c7a506c7f270d9730395a3439ed1
Senfire <eric.mcghee@northwestern.edu>
2022-09-28 14:49:20 -0500

    fixed the conditional for the server connection

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,21 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// let sequelize;
+let sequelize;
 
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    // ! Grayson has to use 127.0.0.1 ... DOUBLE-CHECK THAT YOURS IS SET TO LOCALHOST! 
-    host: '127.0.0.1',
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
-// !}
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
