86fd4889a6d4fdc44bed51a155865039d5a0fc10
Grayson Harvey <graycodes.nu@gmail.com>
2022-09-27 21:45:12 -0500

    these are the changes that we walked through with jen.

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,11 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+// let sequelize;
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -18,6 +17,6 @@ const sequelize = new Sequelize(
     port: 3306,
   }
 );
-}
+// !}
 
 module.exports = sequelize;
