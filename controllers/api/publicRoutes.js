0fd77459bddf1e0e361ab74b24d9edb6245d34f0
Grayson Harvey <graycodes.nu@gmail.com>
2022-09-29 23:46:46 -0500

    ROUTES ROUTES ROUTES ROUTES. Walked through each route and refactored as needed. Created TODOS and notified team.

--- controllers/api/publicRoutes.js
+++ controllers/api/publicRoutes.js
@@ -13,6 +13,26 @@ const { reset } = require("nodemon");
 const strainData = require("../../seeds/strainData.json");
 const db = require('../../config/connection');
 
+// **** MISC PAGE ROUTES ****
+
+// GET signup
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+router.get("/logout", (req, res) => {
+  res.render("verifyAge");
+});
+
+//Get login route
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.get("/myReviews", (req, res) => {
+  res.render("myReviews");
+});
+
 
 // **** AGE VERIFICATION ROUTES ****
 
@@ -32,41 +52,50 @@ router.post("/verifyAge", (req, res) => {
 });
 
 // **** LOGIN ROUTES ****
-// POST login
-router.post("/login", (req, res) => {
-  const { email, password } = req.body;
-  res.redirect('/strains');
+
+// TODO: POST login *REFACTOR*
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.user_name } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+        // res.redirect('/login')
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+        // res.redirect('/login')
+      return;
+    }
+
+    res.redirect('/strains')
+    // req.session.save(() => {
+    //   req.session.user_id = userData.id;
+    //   req.session.logged_in = true;
+
+    //   res.json({ user: userData, message: 'You are now logged in!' });
+    // });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
+// TODO: GET LOGIN
+
 
 // **** SIGNUP ROUTES ****
-// GET signup
-router.get("/signup", (req, res) => {
-  res.render("signup");
-});
 
-router.get("/logout", (req, res) => {
-  res.render("verifyAge");
-});
-
-//Get login route
-router.get("/login", (req, res) => {
-  res.render("login");
-});
-
-router.get("/myReviews", (req, res) => {
-  res.render("myReviews");
-});
 // POST signup
-// router.post("/signup", (req, res) => {
-//   const { fname, lname, username, password } = req.body;
-//   res.send(
-//     `First name : ${fname} 
-//       Last name : ${lname} 
-//       Username : ${username} 
-//       Password : ${password}`
-//   );
-// });
-
   router.post('/signup', async (req, res) => {
     try {
       const newUserData = await User.create({
@@ -83,7 +112,10 @@ router.get("/myReviews", (req, res) => {
     }
   });
 
+
+
 // **** STRAIN ROUTES ****
+
 // GET all strains
 router.get("/strains", (req, res) => {
   Strain.findAll({
@@ -146,28 +178,26 @@ router.get("/reviews", async (req, res) => {
     });
 });
 
-// GET review by ID
-router.get("api/review/:id", (req, res) => {
-  review
-    .findOne({
-      where: {
-        id: req.params.id,
-      },
-    })
-    .then((review) => res.json(review))
-    .catch((error) => res.status(400).json(error));
-});
-// POST review
-router.post("/postReview", (req, res) => {
+// GET review by ID **May not need**
+// router.get("api/review/:id", (req, res) => {
+//   review
+//     .findOne({
+//       where: {
+//         id: req.params.id,
+//       },
+//     })
+//     .then((review) => res.json(review))
+//     .catch((error) => res.status(400).json(error));
+// });
+
+// TODO: GET Route for User Review (ER2)
+
+// TODO: POST review *REFACTOR*
+router.post("/postReview", async(req, res) => {
   try {
-    const { user_id, content, rating, strain_id, title, timestamp } = req.body;
-    res.send(`
-      ${title} 
-      ${rating} 
-      ${strain_id} 
-      ${content}
-      ${user_id}
-      ${timestamp}`);
+    const newReview = await Review.create(req.body);
+    res.status(200).json(newReview)
+
       // res.redirect(allReviews)
   } catch (err) {
     res.status(400).json(err);
@@ -175,6 +205,7 @@ router.post("/postReview", (req, res) => {
 });
 
 // **** FAVORITE ROUTES ****
+
 // GET all favorites
 router.get("/favorites", (req, res) => {
   favorite
@@ -183,7 +214,7 @@ router.get("/favorites", (req, res) => {
     .catch((error) => res.status(400).json(error));
 });
 
-// GET favorites by ID
+// TODO: GET favorites by ID **REFACTOR**
 router.get("api/favorite/:id", (req, res) => {
   favorite
     .findByPk({
@@ -199,34 +230,27 @@ router.get("api/favorite/:id", (req, res) => {
 router.post("/post-favorite", async (req, res) => {
   try {
   const newFav =  await Favorites.create(req.body);
-  //  { strain_id, name, positive_effects, negative_effects, type } = req.body;
-    // res.send(
-    // `${strain_id}
-    // ${name}
-    // ${positive_effects}
-    // ${negative_effects}
-    // ${type}`
-  // );
+
   res.status(200).json(newFav);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-// DELETE favorite
+// TODO DELETE favorite **REFACTOR**
 router.delete('/delete-fav/:id', async (req, res) => {
   // delete a category by its `id` value
   try {
-    const  deletFav = await Category.destroy({
+    const  deletFav = await Favorites.destroy({
       where: {
         id: req.params.id,
       },
     });
-    if (!categoryData) {
+    if (!deletFav) {
       res.status(404).json({ message: 'Invalid' });
       return;
     }
-    res.status(200).json(categoryData);
+    res.status(200).json(deletFav);
   } catch (err) {
     res.status(500).json(err);
   }
