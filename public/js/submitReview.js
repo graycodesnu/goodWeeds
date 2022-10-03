c093032cac6323b902d0f969bb2eb5d12c73d19f
Grayson Harvey <graycodes.nu@gmail.com>
2022-10-03 02:20:59 -0500

    Trying to get strain name to append with review. Need to link strain ID and strain name with user input.

--- public/js/submitReview.js
+++ public/js/submitReview.js
@@ -1,45 +1,11 @@
-// * append dropdown selection
-// function getOption() {
-  // console.log('hey bestie');
+// const { text } = require("express");
 
-  // selectElement = document.querySelector('.select');
-  // console.log(selectElement);
-
-  // var val1 = output = selectElement.value;
-  // console.log(val1);
-
-  // console.log(output1);
-
-  // var x = document.getElementsByClassName("select").value;
-  // console.log(x);
-
-  // document.getElementsByClassName("output").innerHTML = x;
-
-//   var text = selectElement.options[selectElement.selectedIndex].text;
-// console.log(text);
-
-// var output1 = document.querySelector('.output').textContent = "something";
-// console.log(output1);
-// }
-// const strainTitle = document.querySelector('#strains').value;
-// const strain_title = Number(strainTitle);
-//   console.log(strain_title);
 
 // ***** Submit Review Functionality *****
 console.log('connected');
 
 const tag = document.getElementsByTagName('option');
 console.log(tag);
-
-// const languages = document.getElementById('strainOpt');
-// console.log(languages);
-
-// var valueNum = languages.options[languages.selectedIndex].value;
-// console.log("THIS IS A VALUE", strainOpt.value);
-
-// var textVal = languages.options[languages.selectedIndex].innerText;
-// console.log("THIS IS TEXT", strainOpt.innerText);
-
 
 async function reviewSubmit(event) {
   event.preventDefault();
@@ -54,6 +20,9 @@ async function reviewSubmit(event) {
   const userReview = document.querySelector('#content').value;
     console.log(userReview);
   
+  const strainName = Number(document.querySelector('#strains').value);
+    console.log(strainName);
+
   // * Rating Buttons
   // var btnVal = document.getElementById('#rating').value;
   // console.log(btnVal);
@@ -63,7 +32,8 @@ async function reviewSubmit(event) {
     body: JSON.stringify({
       title: revTitle,
       strain_id: strainId,
-      content: userReview
+      content: userReview,
+      strain_name: strainName,
     }),
     headers: {
     'Content-Type': 'application/json',
@@ -71,7 +41,7 @@ async function reviewSubmit(event) {
   })
 
   // if (response.ok) {
-  //   document.location.replace('/allReviews');
+  //   document.location.reload();
   // } else {
   //   alert('failed to add review');
   // }
